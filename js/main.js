2d8d69c40f22db2ca50d0daeb5fd71e08b4f6d59
Luc <lucthemit@gmail.com>
2022-11-16 15:45:34 -0300

    Aula 4: Definição de data-attributes

--- js/main.js
+++ js/main.js
@@ -1,23 +1,19 @@
-const subtrair = document.querySelector('#subtrair')
-const somar = document.querySelector('#somar')
-const braco = document.querySelector('#braco')
-
-const controle = document.querySelectorAll('.controle-ajuste')
-const componente = document.querySelectorAll('.controle-contador')
+const controleAjuste = document.querySelectorAll('[data-controle]')
 
 
-controle.forEach( (elemento) => {
+controleAjuste.forEach( (elemento) => {
     elemento.addEventListener('click', (evento) => {
-        manipulaDados(evento.target.textContent)
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
     })
 }
 )
 
 
-function manipulaDados(operacao) {
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector('[data-contador]')
     if (operacao === '-'){
-        braco.value = parseInt(braco.value) - 1
+        peca.value = parseInt(peca.value) - 1
     } else if (operacao === '+'){
-        braco.value = parseInt(braco.value) + 1
+        peca.value = parseInt(peca.value) + 1
     }
 }
