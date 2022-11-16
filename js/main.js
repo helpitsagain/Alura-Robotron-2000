b9874ebc894770bf8165ee0c94cb75ff11909002
Luc <lucthemit@gmail.com>
2022-11-16 04:16:15 -0300

    Aulas 1 a 3 implementando comportamento dos botões de soma e subtração.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
-const robotron = document.querySelector('#robotron')
+const subtrair = document.querySelector('#subtrair')
+const somar = document.querySelector('#somar')
+const braco = document.querySelector('#braco')
 
-robotron.addEventListener('click', () => {console.log('Clique no robô.');})
+const controle = document.querySelectorAll('.controle-ajuste')
+const componente = document.querySelectorAll('.controle-contador')
 
-function dizOi() {
-    console.log('oi')
+
+controle.forEach( (elemento) => {
+    elemento.addEventListener('click', (evento) => {
+        manipulaDados(evento.target.textContent)
+    })
 }
+)
 
-dizOi()
+
+function manipulaDados(operacao) {
+    if (operacao === '-'){
+        braco.value = parseInt(braco.value) - 1
+    } else if (operacao === '+'){
+        braco.value = parseInt(braco.value) + 1
+    }
+}
