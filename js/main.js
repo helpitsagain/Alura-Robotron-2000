a0dd2835d860a30b7a25839a882952003713739d
Luc <lucthemit@gmail.com>
2022-11-16 03:43:55 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const robotron = document.querySelector('#robotron')
+
+robotron.addEventListener('click', () => {console.log('Clique no robô.');})
+
+function dizOi() {
+    console.log('oi')
+}
+
+dizOi()
